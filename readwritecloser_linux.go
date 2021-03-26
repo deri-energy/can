b7b7d02e9e76fb367174b1d6feabc6fa5bcfe7f9
yangrui <2531725532@qq.com>
2021-03-26 16:04:23 +0800

    增加本地回环

--- readwritecloser_linux.go
+++ readwritecloser_linux.go
@@ -1,6 +1,7 @@
 package can
 
 import (
+	"errors"
 	"fmt"
 	"golang.org/x/sys/unix"
 	"net"
@@ -21,12 +22,12 @@ func NewReadWriteCloserForInterface(i *net.Interface) (ReadWriteCloser, error) {
 	// 打开本地回环
 	err = syscall.SetsockoptInt(s, unix.SOL_CAN_RAW, unix.CAN_RAW_LOOPBACK, 1)
 	if err != nil {
-		return nil, err
+		return nil, errors.New(fmt.Sprintf("本地回环错误%s", err.Error()))
 	}
 
 	err = syscall.SetsockoptByte(s, unix.SOL_CAN_RAW, unix.CAN_RAW_ERR_FILTER, unix.CAN_ERR_TX_TIMEOUT|unix.CAN_ERR_BUSOFF|unix.CAN_ERR_BUSERROR)
 	if err != nil {
-		return nil, err
+		return nil, errors.New(fmt.Sprintf("filter错误%s", err.Error()))
 	}
 
 	f := os.NewFile(uintptr(s), fmt.Sprintf("fd %d", s))
